6f26ee254b72560178add8065493c8d55e01a3fa
unknown <atikurahad@gmail.com>
2024-05-13 14:54:19 +0600

    create and connecting to DB

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const app = express();
 
 const PORT = 3001;
 
+// crate product schema
+const productsSchema= new mongoose.Schema({
+  title: String,
+  
+  price: Number,
+  descreption: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+// create products model 
+
+
+const Product = mongoose.model("products",productsSchema)
+
 const connecDB = async () => {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/test");
