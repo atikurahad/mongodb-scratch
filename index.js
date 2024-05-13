cf7112c608f8b6472da3f56a59ec1c2d9dc14f83
unknown <atikurahad@gmail.com>
2024-05-13 13:12:28 +0600

    connected mongoose with mongodb

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-
-const express = require('express')
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
 const PORT = 3001;
 
-app.get("/",(req,res)=>{
-    res.send("Hi, Welcome to MongoDb")
-})
+const connecDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    console.log("DB is connected");
+  } catch (error) {
+    console.log("DB is not connected");
+    console.log(error.message);
+    process.exit(1);
+  }
+};
 
-app.listen(PORT,()=>{
-    console.log(`Server in running at http://localhost:${PORT}`);
-})
+app.get("/", (req, res) => {
+  res.send("Hi, Welcome to MongoDb");
+});
+
+app.listen(PORT, async () => {
+  console.log(`Server in running at http://localhost:${PORT}`);
+  await connecDB();
+});
